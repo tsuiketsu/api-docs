8f3ed13ef08d7f9f4f7e12a6a4a9807e51579ed5
ImRayy <souradeep_roy@pm.me>
2025-10-19 14:13:47 +0530

    chore: update site title

--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -1,7 +1,6 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
 
 export const openapi = createOpenAPI({
-  // input: [process.env.OPENAPI_JSON_URL!],
-  input: ["http://localhost:8000/openapi.json"],
-  // input: ["./openapi.json"],
+  // biome-ignore lint/style/noNonNullAssertion: false
+  input: [process.env.NEXT_OPENAPI_JSON_URL!],
 });
