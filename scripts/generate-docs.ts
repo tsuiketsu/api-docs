c433609c67011100ecf52265988f0d0e9462c238
ImRayy <souradeep_roy@pm.me>
2025-10-19 12:24:37 +0530

    feat(docs): refactor OpenAPI generation and documentation pages

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,8 +1,21 @@
 import { generateFiles } from "fumadocs-openapi";
+import { rimraf } from "rimraf";
 import { openapi } from "@/lib/openapi";
 
-void generateFiles({
-  input: openapi,
-  output: "./content/docs",
-  includeDescription: true,
-});
+const out = "./content/docs/(api)";
+
+async function generate() {
+  await rimraf(out, {
+    filter(v) {
+      return !v.endsWith("meta.json");
+    },
+  });
+
+  await generateFiles({
+    input: openapi,
+    output: out,
+    groupBy: "tag",
+  });
+}
+
+void generate();
--- src/lib/openapi.ts
+++ src/lib/openapi.ts
@@ -1,6 +1,7 @@
 import { createOpenAPI } from "fumadocs-openapi/server";
 
 export const openapi = createOpenAPI({
-  // biome-ignore lint/style/noNonNullAssertion: false
-  input: [process.env.OPENAPI_JSON_URL!],
+  // input: [process.env.OPENAPI_JSON_URL!],
+  input: ["http://localhost:8000/openapi.json"],
+  // input: ["./openapi.json"],
 });
